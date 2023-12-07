a2ac2795297aae3fa16c4873d85b5808f1dfec6b
A.kafagy <111977961+Tacoao@users.noreply.github.com>
2023-12-07 23:50:13 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+{
+    "questions": [
+      {
+        "id": 1,
+        "text":"blablabla",
+        "answers": [
+          {
+            "id":1 ,
+            "text":"blablabla" ,
+            "Etat": [
+                {
+                    "typeEtat":0,
+                    "Point":2
+                }
+            ]
+
+          }
+        ]
+        }
+    ]
+}
